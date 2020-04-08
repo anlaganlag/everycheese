adfd2ee478c5efd12da98f0c50526179ede682a4
gal <tanjhu@163.com>
2020-04-08 23:17:03 +0800

    forms

--- sql/cheeses/views.py
+++ sql/cheeses/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from .models import Cheese
+from django.views.generic import CreateView
 
 class CheeseListView(ListView):
     model = Cheese
         # Create your views here.
 class CheeseDetailView(DetailView):
     model = Cheese
+class CheeseCreateView(CreateView):
+    model = Cheese
+    fields = ['name', 'description', 'firmness','country_of_origin']
