07f8921e5913b462272c5d45f665bf710cf70400
gal <tanjhu@163.com>
2020-04-09 15:38:56 +0800

    add_creator_field

--- sql/cheeses/models.py
+++ sql/cheeses/models.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from autoslug import AutoSlugField
 from model_utils.models import TimeStampedModel
 from django_countries.fields import CountryField
+from django.conf import settings
 
 class Cheese(TimeStampedModel):
     class Firmness(models.TextChoices):
@@ -17,6 +18,10 @@ class Cheese(TimeStampedModel):
     description = models.TextField("Description",blank=True)
     firmness = models.CharField("Firmness",max_length=20,choices=Firmness.choices,default=Firmness.UNSPECIFIED)
     country_of_origin = CountryField("Country of Origin", blank=True)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL,
+                        null=True,
+                        on_delete=models.SET_NULL
+    )
     def __str__(self):
         return self.name
     def get_absolute_url(self):
