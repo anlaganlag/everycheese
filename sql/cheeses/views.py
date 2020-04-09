5af1c5fa4ccd71c00fbc4e33a5da90a11567875d
gal <tanjhu@163.com>
2020-04-09 15:11:56 +0800

    mixin_logged_in_only

--- sql/cheeses/views.py
+++ sql/cheeses/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from .models import Cheese
 from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class CheeseListView(ListView):
     model = Cheese
         # Create your views here.
 class CheeseDetailView(DetailView):
     model = Cheese
-class CheeseCreateView(CreateView):
+class CheeseCreateView(LoginRequiredMixin,CreateView):
     model = Cheese
     fields = ['name', 'description', 'firmness','country_of_origin']
